4b20bde17b8ffec0a95b4e70f37e82f3bcee6eab
Alexander Fedkin <st087201@student.spbu.ru>
2022-02-18 23:12:43 +0300

    java doesn't contain method random for hashset :(

--- src/main/java/SampleBot.java
+++ src/main/java/SampleBot.java
@@ -3,7 +3,13 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class SampleBot extends TelegramLongPollingBot {
+
+    private final HashSet<String> someUsedWords = new HashSet<>();
 
     @Override
     public String getBotUsername() {
@@ -20,14 +26,26 @@ public class SampleBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             var text = update.getMessage().getText();
             var chatId = update.getMessage().getChatId();
+
+            someUsedWords.add(text);
+
             var messageQuery = new SendMessage();
             messageQuery.setChatId(String.valueOf(chatId));
-            messageQuery.setText(text);
+            messageQuery.setText(getRandomSetElement(someUsedWords));
+
             try {
                 execute(messageQuery);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static <E> E getRandomSetElement(Set<E> set) {
+        return set
+                .stream()
+                .skip(new Random().nextInt(set.size()))
+                .findFirst()
+                .orElse(null);
     }
 }
